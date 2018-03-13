960fc7d64891bf0dedb4a57dda5813991b16fea2
jonhcervera <mega_jon13@hotmail.com>
2018-03-13 12:24:26 -0600

    se agrega froms a tabla inventario

---
+++ proyecto1/inventarioForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyecto1
+{
+    public partial class inventarioForm : Form
+    {
+        public inventarioForm()
+        {
+            InitializeComponent();
+        }
+
+        private void inventariosBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.inventariosBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.basedatos1DataSet);
+
+        }
+
+        private void inventarioForm_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'basedatos1DataSet.inventarios' Puede moverla o quitarla según sea necesario.
+            this.inventariosTableAdapter.Fill(this.basedatos1DataSet.inventarios);
+
+        }
+    }
+}
